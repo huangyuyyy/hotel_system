4aa0c12b256344427c4cbe4b7a37fa36bf29dedc
HuangYu <huangyuyyy@163.com>
2021-05-21 16:08:55 +0800

    5.21 commit

--- src/main/java/cn/huangyy/hotelsys/service/impl/RoomInfoServiceImpl.java
+++ src/main/java/cn/huangyy/hotelsys/service/impl/RoomInfoServiceImpl.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 @Service
 @Transactional
 public class RoomInfoServiceImpl extends ServiceImpl<RoomInfoMapper, RoomInfo> implements RoomInfoService {
